be02f8cdeca91fd6c18e45d64fca234da996baef
sainadendla <saignadendla@gmail.com>
2018-08-15 23:24:54 -0700

    tutorial controllers

---
+++ MidKnight/Controllers/TutorialPageFour.swift
@@ -0,0 +1,21 @@
+//
+//  TutorialPageFour.swift
+//  MidKnight
+//
+//  Created by Sai Nadendla on 8/15/18.
+//  Copyright © 2018 Sai Nadendla. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TutorialPageFour: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+}
+
