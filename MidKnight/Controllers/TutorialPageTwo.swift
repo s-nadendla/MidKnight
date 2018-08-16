8fee89d6c2d757ab1af88fd88e7298204b1b6cc4
sainadendla <saignadendla@gmail.com>
2018-08-15 23:23:01 -0700

    tutorialpageuichanges

---
+++ MidKnight/Controllers/TutorialPageTwo.swift
@@ -0,0 +1,21 @@
+//
+//  TutorialPageTwo.swift
+//  MidKnight
+//
+//  Created by Sai Nadendla on 8/15/18.
+//  Copyright © 2018 Sai Nadendla. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class TutorialPageTwo: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+}
