d5a6ad7a2e2de825bea6dad041fe49b10f9716e7
sainadendla <saignadendla@gmail.com>
2018-08-23 23:21:35 -0700

    finished on boarding

---
+++ MidKnight/Controllers/HorizontalSlideBack.swift
@@ -0,0 +1,32 @@
+//
+//  HorizontalSlide.swift
+//  MidKnight
+//
+//  Created by Sai Nadendla on 8/23/18.
+//  Copyright © 2018 Sai Nadendla. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HorizontalSlideBack: UIStoryboardSegue {
+    override func perform() {
+        let src = self.source
+        let dst = self.destination
+        
+        src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
+        dst.view.transform = CGAffineTransform(translationX: -src.view.frame.size.width, y: 0)
+        
+        UIView.animate(withDuration: 0.25,
+                       delay: 0.0,
+                       options: .curveEaseInOut,
+                       animations: {
+                        dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
+                        
+        },
+                       completion: { finished in
+                        src.present(dst, animated: false, completion: nil)
+        }
+        )
+    }
+}
