6b60970042cbe1d4b96d22c4ad2fedda3340583b
sainadendla <saignadendla@gmail.com>
2018-08-25 02:49:26 -0700

    Settings UI

--- MidKnight/Controllers/SettingsPage.swift
+++ MidKnight/Controllers/SettingsPage.swift
@@ -12,11 +12,18 @@ import UIKit
 
 class SettingsPage: UIViewController {
     
-    
+    @IBOutlet var settingsRectangle: [UIView]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        for individualRectangle in self.settingsRectangle {
+            individualRectangle.layer.borderWidth = 1
+            individualRectangle.layer.borderColor = UIColor(red:0.41, green:0.41, blue:0.41, alpha: 1).cgColor
+        }
+        
     }
+
     
 }
